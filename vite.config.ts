b3cb082ba53eff28799a167ecc988dd7313d4009
gabrielforster <rochafrgabriel@gmail.com>
2022-06-08 23:50:43 -0300

    core: server configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3333,
+    open: true,
+  },
   plugins: [react()]
 })
